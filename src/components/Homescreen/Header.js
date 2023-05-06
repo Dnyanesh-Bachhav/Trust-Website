ca512fbcbdd4ef94f56a278a2a137f2c61059de6
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2023-05-06 06:27:14 +0530

    insert route updated...

--- src/components/Homescreen/Header.js
+++ src/components/Homescreen/Header.js
@@ -1,4 +1,21 @@
+import axios from "axios";
+
 function Header() {
+    const handleSubmit = async (e)=>{
+        console.log("In a function");
+        e.preventDefault();
+        console.log("Called...");
+        axios.post('/api/setGallery',{
+            name: "Photo 4",
+            img_src: "photo4.png",
+            index: 4
+        }).then((res)=>{
+            console.log("Response: ");
+            console.log(res);
+        }).catch(e=>{
+            console.log("Error: "+e);
+        });
+    }
     return (
         <div className="header-container">
             <ul className="header-ul">
@@ -9,6 +26,11 @@ function Header() {
                 <li><a href="#findus">FIND US</a><p>+</p></li>
                 <li><a href="#about">ABOUT US</a><p>+</p></li>
             </ul>
+
+            <form action="" method="post" onSubmit={handleSubmit}>
+                <input type="text" name="name" id="" />
+                <button type="submit" >Submit</button>
+            </form>
         </div>
     );
 }
--- src/pages/api/setGallery.js
+++ src/pages/api/setGallery.js
@@ -1,23 +1,7 @@
-import { getPhotos, setPhotos } from "../../../lib/mongo/images";
+import { setPhotos } from "../../../lib/mongo/images";
 
 const handler = async (req,res)=>{
-    // GET
-    if(req.method==="GET")
-    {
-        try{
-            const { photos, error } = await getPhotos();
-            if(error) throw new Error(error);
-
-            return res.status(200).json({ photos })
-        }
-        catch(error){
-            return res.status(500).json({
-                error: error.message
-            })
-        }
-        res.setHeader('Allow',['GET'])
-        res.status(425).end(`method ${req.method} is not allowed.`)
-    }
+    
     // POST
     if(req.method==="POST")
     {
